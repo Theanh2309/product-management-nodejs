b3187c0b54cf9e65d0e39b9ace4187e23c4967d6
theanh <theanhb123092002@gmail.com>
2024-12-03 15:31:27 +0700

    cofig env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // cau hinh express
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // import routes
 const route = require("./routes/client/index.route");
