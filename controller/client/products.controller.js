e11a1efc928ece300eb62fb164bf1967cdd6c79f
theanh <theanhb123092002@gmail.com>
2024-12-04 12:14:20 +0700

    dssp client

--- controller/client/products.controller.js
+++ controller/client/products.controller.js
@@ -1,5 +1,23 @@
+const Product = require("../../models/product.model");
+// controller co nhiem vu xu ly cac logic
 module.exports.index = async (req, res) => {
+  // render theo dieu kien(filter)
+  const products = await Product.find({
+    // bo loc
+    status: "active",
+    deleted: false,
+  });
+  // console.log(products);
+  // logic them 1 filed newprice
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed();
+  });
+
   res.render("client/page/products/index.pug", {
     pageTitle: "DS san pham",
+    products: newProducts,
   });
 };
