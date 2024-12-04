a4b272d190158a3e019732ef6aec6b923e056fed
theanh <theanhb123092002@gmail.com>
2024-12-04 23:57:50 +0700

    lay data sp in ra giao dien admin

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,9 +1,13 @@
 // [GET] /admin/products
-
+const Product = require("../../models/product.model");
 // export ten controller(dashboard)
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
   res.render("admin/page/products/index.pug", {
     pageTitle: "trang san pham admin",
+    products: products,
   });
   // res.send("DSSP ADMIN");
 };
--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,3 +1,5 @@
+// [GET] /
+
 module.exports.index = (req, res) => {
   // controller: lay data tra ve view
   res.render("client/page/home/index.pug", { pageTitle: "trang chu" });
