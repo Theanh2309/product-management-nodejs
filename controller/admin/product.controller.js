6e8e8f6df3db1bdf23c14f4a8fa2e9fd7ea3ea0f
theanh <theanhb123092002@gmail.com>
2024-12-05 10:44:46 +0700

    filter status

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -2,12 +2,22 @@
 const Product = require("../../models/product.model");
 // export ten controller(dashboard)
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  // console.log(req.query.status);
+  let find = {
     deleted: false,
-  });
+    // truyen thang vaof day thi ko if else dc
+    // status: req.query.status
+  };
+  // khong phai luc nao cung truy van status nen ta de ben ngoai obj find de co the if else
+  // add key
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  // truong Hợp chọn all thì ko truyền params hoặc truyền string rỗng để underfine nên câu điều kiện ìf ko chạy vào thì nó sẽ lấy tất cả trong db
+
+  const products = await Product.find(find);
   res.render("admin/page/products/index.pug", {
     pageTitle: "trang san pham admin",
     products: products,
   });
-  // res.send("DSSP ADMIN");
 };
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,27 @@
 console.log("OK SCRIPT");
+
+// button status(thuoc tinh tu dinh nghia them dau ngoac vuong)
+const buttonsStatus = document.querySelectorAll("[button-status]");
+
+// console.log(buttonsStatus);
+if (buttonsStatus.length > 0) {
+  // dung new URl de co cac ham (function) de phan tich url trinh duyet(VD: searchParams)
+  let url = new URL(window.location.href);
+
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      // console.log(status);
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        // tat ca
+        url.searchParams.delete("status");
+      }
+
+      console.log(url.href);
+      // redirect
+      window.location.href = url.href;
+    });
+  });
+}
