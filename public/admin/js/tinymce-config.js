e6d811cc80ef142643ca834623b74bba7a252429
theanh <theanhb123092002@gmail.com>
2024-12-15 17:58:19 +0700

    tinymce config

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,5 +1,6 @@
 // khoi tao tinycme
 tinymce.init({
-  selector: 'textarea',
+  selector: 'textarea#content',
+  // cần cái nào thì add 1 class hoặc 1 id chung(.content)
   plugins: 'lists link image table code help wordcount'
 });
