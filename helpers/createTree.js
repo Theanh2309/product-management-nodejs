3ec787c9d85a0bba2374a16583c6ec0998c1a660
theanh <theanhb123092002@gmail.com>
2024-12-17 13:38:44 +0700

    index table products

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,9 +1,12 @@
+let count = 0
 const createTree = (arr, parentId = "")=>{
   const tree = [];
   arr.forEach(item => {
     // So sánh dạng chuỗi
     if (String(item.parent_id) === String(parentId)) {
+      count++
       const newItem = { ...item }; // Tạo bản sao object
+      newItem.index = count;
       const children = createTree(arr, String(item._id)); 
       // Truyền `_id` dưới dạng chuỗi
       if (children.length > 0) {
@@ -14,7 +17,10 @@ const createTree = (arr, parentId = "")=>{
   });
   return tree;
 }
+
 module.exports.tree =(arr, parentId = "")=>{
+  count =0;
 const tree = createTree(arr, parentId = "");
+
 return tree;
 }
